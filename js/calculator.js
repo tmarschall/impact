b1aba2e49bcc0142dfa8da352b68a779fff17f74
Theodore Marschall <theodore.marschall@gmail.com>
2014-12-01 23:07:51 -0500

    update default bahavior

--- js/calculator.js
+++ js/calculator.js
@@ -1,55 +1,30 @@
 (function(){
 	var charities = [];
-<<<<<<< HEAD
-	var jqXHR = $.getJSON('json/charities.json', function( data ) {
-    	charities = data.charities;
-    	        		
-       	$('#sci').prop('checked', true);
-  		$('#amount').val("100");
-=======
 	var jqXHR = $.getJSON('/impact/json/charities.json', function( data ) {
     	charities = data.charities;
 
     	$('#amount').val("100");
-<<<<<<< HEAD
     	var charity = getCharityById(charities, 'sci');
     	updateCharity(charity);
     	updateImpacts(charity, 100);
-=======
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
-  		var charity = getCharityById(charities, 'sci');
-		updateCharity(charity);
-  		updateImpacts(charity, 100);
->>>>>>> master
 	})
   	.fail(function(data, textStatus, error) {
        	console.error("Could not load charities.json, status: " + textStatus + ", error: " + error);
   	});
-<<<<<<< HEAD
-		
-=======
 
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
+
 	var getCharityById = function(charities, id) {
 		for (var i = 0; i<charities.length; i++) {
 			if (charities[i].id == id) return charities[i];
 		}
 		return charities[0];
 	};
-<<<<<<< HEAD
-			
-=======
-
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
+	
 	var calculateImpacts = function(charity, donation) {
 		var impacts = [];
 		var overheadMultiplier = 1.0 - charity.overhead;
 		var usableDonation = overheadMultiplier*donation;
-<<<<<<< HEAD
-		for (i = 0; i < charity.pricePoints.length; i++) {	
-=======
 		for (i = 0; i < charity.pricePoints.length; i++) {
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
 			pp = charity.pricePoints[i];
 			if (usableDonation >= pp.price) {
 				impacts.push({number: Math.floor(usableDonation/pp.price), action: pp.action, item: pp.item});
@@ -57,11 +32,7 @@
 		}
 		return impacts;
 	};
-<<<<<<< HEAD
-			
-=======
-
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
+	
 	var updateImpacts = function(charity, donation) {
 		var impacts = calculateImpacts(charity, donation);
 		var n = impacts.length;
@@ -81,11 +52,7 @@
 			$(resultId).hide();
 		}
 	};
-<<<<<<< HEAD
-			
-=======
-
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
+	
 	var updateCharity = function(charity) {
 		$('#donate span').html(charity.name);
 		$('#learnmore span').html(charity.name);
@@ -93,26 +60,7 @@
 		$('#numbers p').html(charity.numbers);
 		$('#recommendation p').html(charity.recommendation);
 	};
-<<<<<<< HEAD
   			
-	$('input:radio[name=charity]').change( function() {
-		var charity = getCharityById(charities, this.id);
-		var donation = parseInt($('#amount').val()) || 0;
-		
-		updateCharity(charity);
-		updateImpacts(charity, donation);
-	});
-			
-	$('#amount').change( function() {
-		var charId = $('input:radio[name=charity]:checked')[0].id;
-		var charity = getCharityById(charities, charId);
-		var donation = parseInt($('#amount') .val()) || 0; 
-		
-		updateImpacts(charity, donation);
-	});
-			
-=======
-
 	$('input:radio[name=charity]').change( function() {
 		var charity = getCharityById(charities, this.id);
 		var donation = parseFloat($('#amount').val()) || 0;
@@ -131,30 +79,16 @@
 		$(document).trigger('contentChange');
 	});
 
-
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
 	$('#amount').keypress( function(e) {
 		if (e.which == 13) {
 			e.preventDefault();
 			var charId = $('input:radio[name=charity]:checked')[0].id;
 			var charity = getCharityById(charities, charId);
-<<<<<<< HEAD
 			var donation = parseFloat($('#amount') .val()) || 0;
-=======
-			var donation = parseInt($('#amount') .val()) || 0;
-<<<<<<< HEAD
-				
-			updateImpacts(charity, donation);
-		}
-	});
-	
-=======
->>>>>>> master
-
+			
 			updateImpacts(charity, donation);
 			$(document).trigger('contentChange');
 		}
 	});
-
->>>>>>> 799750be285a64a02eddfe185798afc5c5d39021
+	
 }());
